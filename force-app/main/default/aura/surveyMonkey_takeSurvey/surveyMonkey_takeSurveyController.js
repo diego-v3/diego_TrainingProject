5680de368a1d4c49abd21181f8b330f5f687220d
Diego E Vega <diego@appiphony.com>
2019-07-31 08:58:51 -0500

    Added aoppex class and classes folder

--- force-app/main/default/aura/surveyMonkey_takeSurvey/surveyMonkey_takeSurveyController.js
+++ force-app/main/default/aura/surveyMonkey_takeSurvey/surveyMonkey_takeSurveyController.js
@@ -4,5 +4,8 @@
 	},
     productReviewButtonClick : function(component, event, helper) {
 		component.set('v.productReviewIsOpen', !component.get('v.productReviewIsOpen'));
+	},
+	populateSurvey : function(component, event, helper) {
+//		Survey[] sur = new [SELECT Name FROM Question__c]
 	}
 })
