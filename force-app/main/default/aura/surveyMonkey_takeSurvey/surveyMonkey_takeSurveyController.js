74beb96fb50ed93b8629eb40604170bc3dd116c5
Diego E Vega <diego@appiphony.com>
2019-08-05 16:48:20 -0500

    diego: training project: part 2 completed, missing submit

--- force-app/main/default/aura/surveyMonkey_takeSurvey/surveyMonkey_takeSurveyController.js
+++ force-app/main/default/aura/surveyMonkey_takeSurvey/surveyMonkey_takeSurveyController.js
@@ -1,11 +1,26 @@
 ({
-	customerSurveyButtonClick : function(component, event, helper) {
-		component.set('v.customerSurveyIsOpen', !component.get('v.customerSurveyIsOpen'));
-	},
-    productReviewButtonClick : function(component, event, helper) {
-		component.set('v.productReviewIsOpen', !component.get('v.productReviewIsOpen'));
-	},
-	populateSurvey : function(component, event, helper) {
-//		Survey[] sur = new [SELECT Name FROM Question__c]
-	}
+    doInit: function(component, event, helper) {
+        var surveyId = component.get('v.surveyId');
+
+        var action = component.get('c.getInitialData');
+        action.setParams({
+            'surveyId': surveyId
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state == 'SUCCESS') {
+                var data = response.getReturnValue();
+                var surveyName = data.surveyName;
+                var questions = data.questions;
+
+                component.set('v.surveyName', surveyName);
+                component.set('v.questions', questions);
+                //component.set('v.sObjList', a.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    submit: function(component, event, helper) {
+        alert("You clicked: " + event.getSource().get("v.label"));
+    }
 })
